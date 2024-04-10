7c7b4e5d2f7577546210ea05fd0a9c0b8eb24f07
McQuest-72 <mcquest2347@gmail.com>
2024-04-10 05:50:18 -0800

    a new program that calculates students highest scores was created

---
+++ day-5-2-exercise-Highest_score.py
@@ -0,0 +1,10 @@
+
+'''A program that calculates the highest score list of scores'''
+
+print()
+student_scores = input('Input a list of student scores\n').split()
+
+for n in range(o, len(student_scores)):
+    student_scores[n] = int(student_scores)
+print(student_scores)
+
